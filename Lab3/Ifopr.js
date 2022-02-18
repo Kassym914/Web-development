2b271a5bac7b96a69c227704c0a67e1c2901f2a1
Kassym914 <79728819+Kassym914@users.noreply.github.com>
2022-02-18 20:37:18 +0600

    Add files via upload

---
+++ Lab3/Ifopr.js
@@ -0,0 +1,10 @@
+let value = prompt('Type a number', "");
+
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+let result = (a + b < 4) ? 'Below' : 'Over';
---
+++ Lab3/Variables.js
@@ -0,0 +1,8 @@
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+let Planet;
+Planet = "Earth";
+alert(Planet);
+let UserName = "User1";
